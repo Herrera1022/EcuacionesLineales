ba864786c14d761f8d7b5ce058cee8a1d27bb046
Juan Pablo <pablo.herrera1@udea.edu.co>
2023-10-05 15:10:42 -0500

    se agregan algunas funciones

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,198 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textFun1 = new System.Windows.Forms.TextBox();
+            this.textFun2 = new System.Windows.Forms.TextBox();
+            this.Resolver = new System.Windows.Forms.Button();
+            this.Nuevo = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textfin3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(339, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(383, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SISTEMA DE ECUACIONES 2X2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(345, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese la primera ecuación de la forma y=mx+b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(354, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ingrese la segunda ecuación de la forma y=mx+b";
+            // 
+            // textFun1
+            // 
+            this.textFun1.Location = new System.Drawing.Point(16, 131);
+            this.textFun1.Name = "textFun1";
+            this.textFun1.Size = new System.Drawing.Size(206, 26);
+            this.textFun1.TabIndex = 3;
+            // 
+            // textFun2
+            // 
+            this.textFun2.Location = new System.Drawing.Point(16, 234);
+            this.textFun2.Name = "textFun2";
+            this.textFun2.Size = new System.Drawing.Size(206, 26);
+            this.textFun2.TabIndex = 4;
+            // 
+            // Resolver
+            // 
+            this.Resolver.Location = new System.Drawing.Point(16, 527);
+            this.Resolver.Name = "Resolver";
+            this.Resolver.Size = new System.Drawing.Size(130, 34);
+            this.Resolver.TabIndex = 5;
+            this.Resolver.Text = "Resolver";
+            this.Resolver.UseVisualStyleBackColor = true;
+            this.Resolver.Click += new System.EventHandler(this.Resolver_Click);
+            // 
+            // Nuevo
+            // 
+            this.Nuevo.Location = new System.Drawing.Point(163, 528);
+            this.Nuevo.Name = "Nuevo";
+            this.Nuevo.Size = new System.Drawing.Size(130, 33);
+            this.Nuevo.TabIndex = 6;
+            this.Nuevo.Text = "Nuevo";
+            this.Nuevo.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(30, 340);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Valor x";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(32, 415);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 23);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Valor y";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(141, 340);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(141, 412);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 10;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(475, 92);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(492, 263);
+            this.chart1.TabIndex = 11;
+            this.chart1.Text = "chart1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(582, 396);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(273, 23);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "¿Perpendicular o paralela?";
+            // 
+            // textfin3
+            // 
+            this.textfin3.Location = new System.Drawing.Point(586, 438);
+            this.textfin3.Name = "textfin3";
+            this.textfin3.Size = new System.Drawing.Size(269, 26);
+            this.textfin3.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1137, 587);
+            this.Controls.Add(this.textfin3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Nuevo);
+            this.Controls.Add(this.Resolver);
+            this.Controls.Add(this.textFun2);
+            this.Controls.Add(this.textFun1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textFun1;
+        private System.Windows.Forms.TextBox textFun2;
+        private System.Windows.Forms.Button Resolver;
+        private System.Windows.Forms.Button Nuevo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textfin3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,53 @@ namespace EcuacionesLneales
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Resolver_Click(object sender, EventArgs e)
+        {
+            if (textFun1.Text == "" && textFun2.Text == "")
+            {
+                MessageBox.Show("Para poder realizar la operación, ingrese " +
+                    "las ecuaciones en los espacios designados.");
+
+            }
+            else
+            {
+                string función1 = textFun1.Text;
+                string función2 = textFun2.Text;
+                string funciónSinEspacios1 = función1.Replace(" ", "");
+                string funciónSinEspacios2 = función2.Replace(" ", "");
+                int indexM = (funciónSinEspacios1.IndexOf("=") + 1);
+                int indexX = (funciónSinEspacios1.IndexOf("x") - 1);
+                int indexM2 = (funciónSinEspacios2.IndexOf("=") + 1);
+                int indexX2 = (funciónSinEspacios2.IndexOf("x") - 1);
+                string m1 = (funciónSinEspacios1.Substring(indexM,indexX));
+                string m2 = (funciónSinEspacios2.Substring(indexM2,indexX2));
+                float pend1 = 0;
+                float pend2 = 0;
+                bool result1=(float.TryParse(m1, out pend1));
+                bool result2 = (float.TryParse(m2, out pend2));
+                if(result1==false && result2==false)
+                {
+                    MessageBox.Show("Por favor ingrese un numero que corresponda al valor de la pendiente m" +
+                        " si el valor de la pendiente es cero o uno por favor ingrese ese valor en la función");
+                }
+                if(result1 && result2)
+                {
+                    if (pend1 * pend2 == -1)
+                    {
+                        textfin3.Text = ("PERPENDICULAR");
+                    }
+                    if (pend2 == pend1)
+                    {
+                        textfin3.Text = ("PARALELA");
+                    }
+                }
+            }
+        }
     }
 }
